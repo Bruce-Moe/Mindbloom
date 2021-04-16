0950d4bcc58168e8a5d421f496a73b92f19be505
Bruce Moe <brucemoe1@gmail.com>
2021-04-15 20:15:33 -0500

    Began working on homescreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,8 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  TextInput,
-} from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import AppLoading from "expo-app-loading";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+import { FontAwesome5 } from "@expo/vector-icons";
 import {
   useFonts,
   Lora_400Regular,
@@ -25,15 +20,48 @@ export default function HomeScreen(props) {
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
-    return <View style={styles.container}></View>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <View style={styles.inputWrap}>
+            <Image
+              style={styles.logo}
+              source={require("../assets/mindbloom_Logo.png")}
+            />
+          </View>
+          <View style={styles.inputWrap}>
+            <TouchableOpacity style={styles.menuIcon}>
+              <FontAwesome5 name="bars" size={24} color="#161924" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "#ECF2F8",
+  },
+  row: {
+    marginTop: getStatusBarHeight(),
+    flex: 1,
+    flexDirection: "row",
+  },
+  inputWrap: {
+    flex: 1,
+  },
+  logo: {
+    width: 134.53,
+    height: 71.5,
+    marginLeft: 10,
+  },
+  menuIcon: {
+    alignItems: "flex-end",
+    marginTop: 22,
+    marginRight: 10,
+    color: "#838EB1",
   },
 });
